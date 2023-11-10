0be96a53209224bbba5c130d8aa5a20ca67ba13a
“Iryna <maria14trush@gmail.com>
2023-11-10 13:28:27 -0800

    update tests file

--- test_server.py
+++ test_server.py
@@ -22,5 +22,19 @@ class TestSummarize(unittest.TestCase):
 
         self.assertIn('error', response.json)
 
+    def test_summary_length(self):
+        text = "This is a sample input text for summarization."
+
+        data = {'text': text}
+
+        with server.app.test_client() as client:
+            response = client.post('/', json=data)
+
+        summary = response.json['summary']
+        max_length = 120
+        min_length = 40
+        self.assertGreaterEqual(len(summary), min_length)
+        self.assertLessEqual(len(summary), max_length)
+
 if __name__ == '__main__':
     unittest.main()
